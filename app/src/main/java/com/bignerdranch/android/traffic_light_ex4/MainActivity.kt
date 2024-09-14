adb3c169431731dc78b2c389954d72539d1a97b1
emmat <emma.trowbridgee@gmail.com>
2024-09-14 16:11:33 -0500

    V2 - Works

--- app/src/main/java/com/bignerdranch/android/traffic_light_ex4/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/traffic_light_ex4/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.bignerdranch.android.traffic_light_ex4
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bignerdranch.android.traffic_light_ex4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    enum class TrafficLightState {
+        STOP, GO, WAIT
+    }
+
+    private var currentState = TrafficLightState.STOP // Start with STOP state
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+
+        updateTrafficLight()
+
+        binding.trafficButton.setOnClickListener {
+            currentState = when (currentState) {
+                TrafficLightState.STOP -> TrafficLightState.GO
+                TrafficLightState.GO -> TrafficLightState.WAIT
+                TrafficLightState.WAIT -> TrafficLightState.STOP
+            }
+            updateTrafficLight()
+        }
+    }
+
+    private fun updateTrafficLight() {
+        when (currentState) {
+            TrafficLightState.STOP -> {
+                binding.trafficButton.text = "STOP"
+                binding.trafficButton.setBackgroundColor(Color.RED)
+
+                binding.trafficlightRed.visibility = View.VISIBLE
+                binding.trafficlightGreen.visibility = View.INVISIBLE
+                binding.trafficlightYellow.visibility = View.INVISIBLE
+            }
+
+            TrafficLightState.GO -> {
+                binding.trafficButton.text = "WAIT"
+                binding.trafficButton.setBackgroundColor(Color.GREEN)
+
+                binding.trafficlightRed.visibility = View.INVISIBLE
+                binding.trafficlightGreen.visibility = View.VISIBLE
+                binding.trafficlightYellow.visibility = View.INVISIBLE
+            }
+
+            TrafficLightState.WAIT -> {
+                binding.trafficButton.text = "WAIT"
+                binding.trafficButton.setBackgroundColor(Color.YELLOW)
+
+                binding.trafficlightRed.visibility = View.INVISIBLE
+                binding.trafficlightGreen.visibility = View.INVISIBLE
+                binding.trafficlightYellow.visibility = View.VISIBLE
+            }
         }
     }
 }
